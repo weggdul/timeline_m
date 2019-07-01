7e4e8b1521b640c37fc94e0a169957995ea8d7da
we dul <wedul.chul@gmail.com>
2019-07-01 23:40:47 +0900

    components 추가

---
+++ components/TimeLineHeader.vue
@@ -0,0 +1,15 @@
+<template>
+    <header>header</header>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style>
+
+</style>
+
+
--- src/App.vue
+++ src/App.vue
@@ -1,60 +1,41 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+  <div id = "app">
+    <TimeLineHeader></TimeLineHeader>
+    <TimeLineList></TimeLineList>
+    <TimeLineFooter></TimeLineFooter>
   </div>
 </template>
 
 <script>
+
+import TimeLineFooter from '../components/TimeLineFooter.vue'
+import TimeLineHeader from '../components/TimeLineHeader.vue'
+import TimeLineList from '../components/TimeLineList.vue'
+
 export default {
-  name: 'app',
-  data () {
-    return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+
+  components: {
+    'TimeLineFooter': TimeLineFooter,
+    'TimeLineHeader': TimeLineHeader,
+    'TimeLineList': TimeLineList
   }
 }
 </script>
 
-<style lang="scss">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
+<style>
+  body {
+        text-align: center;
+        background-color: #f6f6f8;
+    }
+    input {
+        border-style: groove;
+        width: 200px;
+    }
+    button {
+        border-style: groove;
+    }
+    .shadow {
+        box-shadow: 5px 10px 10px rgba(0, 0, 0, 0.03)
+    }
 </style>
+
