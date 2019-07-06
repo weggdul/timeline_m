86bae7af59791cad103f474ecbdb1d50e824d3a9
we dul <wedul.chul@gmail.com>
2019-07-07 00:51:22 +0900

    timeline item ui 추가

--- components/TimeLineList.vue
+++ components/TimeLineList.vue
@@ -1,9 +1,27 @@
 <template>
     <div id="items">
         <section>
-            <transition-group name="list" tag="ul">
-                <li v-for="(title, itemId) in propsdata" :key="itemId" class="shadow timeline-list">
-                    {{ title }}
+            <transition-group name="list" class="timeLineList" tag="ul">
+                <li v-for="(timeLineItem, itemId) in propsdata" :key="itemId" class="shadow timeline-list">
+                    <div id = "itemInner"> 
+                        <div id = "itemInnerBox">
+                            <div id = "logo" v-bind:style="{ backgroundImage: 'url(' + timeLineItem.logoUrl + ')' }">
+                            </div>
+                            <div class="names">                      
+                                <div class="own">
+                                    {{ timeLineItem.title.trim() }}
+                                </div>
+                                <div class="own">
+                                    Update Date : {{ dateFormatting(timeLineItem.updateAt) }}
+                                </div>
+                            </div>
+                        </div>
+                        <div id = "itemInnerBox">
+                            <p>
+                            {{ timeLineItem.content }}
+                            </p>
+                        </div>
+                    </div>
                 </li>
             </transition-group>
     </section>
@@ -11,13 +29,49 @@
 </template>
 
 <script>
+const dateformat = require('dateformat');
+
 export default {
-    props: ['propsdata']        
+    props: ['propsdata'],
+    methods: {
+        dateFormatting(input) {
+            return dateformat(new Date(input), 'yyyy.mm.dd');
+        }
+    }
 }
 </script>
 
 <style>
-    .timeline-list {
+    .own {
+        font-size: 13px;
+        line-height: 23px;
+    }
+
+    .names {
+        display: table-cell;
+        vertical-align: middle;
+        padding-left: 10px;
+        color: #000000;
+    }
+
+    #logo {
+        display: table-cell;
+        width: 50px;
+        height: 50px;
+        background-size: 80%;
+        background-repeat: no-repeat;
+        background-position: center center;
+        border: 1px #ebebeb solid;
+    }
+
+    #itemInnerBox {
+        overflow: hidden;
+        display: inherit;
+        width: 100%;
+        height: 50%;
+    }
+
+    #itemInner {
         overflow: hidden;
     }
 
@@ -46,10 +100,11 @@ export default {
     }
 
     li {
+        text-align: left;
         display: flex;
-        min-height: 50px;
-        height: 50px;
-        line-height: 50px;
+        min-height: 104px;
+        height: 104px;
+        line-height: 104px;
         margin: 0.5rem 0;
         padding: 0 0.9rem;
         background: white;
