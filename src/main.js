3baf84dbddf70c6648400e046b42fa9823fa0cb9
Jayden <jayden@chequer.io>
2019-07-15 00:05:04 +0900

    vue-analytics 추가

--- src/main.js
+++ src/main.js
@@ -1,7 +1,15 @@
 import Vue from 'vue'
 import App from './App.vue'
+import VueAnalytics from 'vue-analytics'
 
 new Vue({
     el: '#app',
     render: h => h(App)
+})
+
+Vue.use(VueAnalytics, {
+    id: 'UA-143819848-1',
+    autoTracking: {
+      screenview: true
+    }
 })
