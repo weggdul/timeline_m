3fcd5ee85ddb07ac9fe02acd903b0d7ba241f47d
Jayden <jayden@chequer.io>
2019-07-15 10:59:11 +0900

    vue-analytics 설정 변경

--- src/main.js
+++ src/main.js
@@ -2,14 +2,21 @@ import Vue from 'vue'
 import App from './App.vue'
 import VueAnalytics from 'vue-analytics'
 
-new Vue({
-    el: '#app',
-    render: h => h(App)
-})
+const isProd = process.env.NODE_ENV === 'production'
 
 Vue.use(VueAnalytics, {
     id: 'UA-143819848-1',
     autoTracking: {
-      screenview: true
+        screenview: true,
+        page: isProd
+    },
+    debug: {
+        enabled: !isProd,
+        sendHitTask: isProd
     }
+})
+
+new Vue({
+    el: '#app',
+    render: h => h(App)
 })
